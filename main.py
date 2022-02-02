7d21bb91a48dd3949cff9d4b272f949680e7fdf2
matthew bahloul <matthew.bahloul@gmail.com>
2022-02-01 16:56:27 -0800

    updates

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
     args = _parse_args()
 
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--disable-dev-shm-usage')
